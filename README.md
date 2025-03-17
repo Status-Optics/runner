626f253d4126c14aab891ba8617ec421e42eebe2
Drew Buckman <drew.buckman@gmail.com>
2025-03-17 09:34:44 -0400

    basic poc

--- README.md
+++ README.md
@@ -1,51 +1,77 @@
 # Status-Optics runner
 This is the runner for the Status-Optics project. The runner is responsible for pulling the test code from git, running the tests, and reporting the results back to the defined endpoint(s).
 
+## Environment Variables needed to bootstrap the runner
+RUNNER_CONFIG_REPO - The URL of the git repository containing the runner configuration.
+RUNNER_CONFIG_BRANCH - The branch of the git repository containing the runner configuration.
+RUNNER_CONFIG_PATH - The path to the runner configuration file in the git repository.
+
+
 ## Runner Configuration
 ```yaml
 # Runner Configuration (runner.yaml)
 
 runner:
-  name: "my-app-monitor-runner" # Optional: A descriptive name for the runner
-  version: "1.0.0" # Optional: Runner version
+  name: "my-app-monitor-runner"
+  version: "1.0.0"
 
-git:
-  repository: "https://github.com/your-org/your-test-repo.git" # Required: Git repository URL
-  branch: "main" # Optional: Git branch (default: main)
-  pull_interval: "60s" # Required for cron-based polling (e.g., "1m", "30s", "1h")
-  webhook_enabled: false # Optional: Enable webhook support (default: false)
-  webhook_secret: "your-secret-webhook-token" # Required if webhook_enabled: true
-
-test:
-  language: "python" # Required: Language of the test scripts (e.g., "python", "javascript", "bash")
-  command: "pytest tests/" # Required: Command to execute the tests (e.g., "pytest", "npm test", "sh run_tests.sh")
-  working_directory: "/app/tests" # Optional: Working directory for test execution
+tests:
+  - name: "api-health-check"
+    language: "python"
+    command: "pytest tests/api_health_test.py"
+    working_directory: "/app/tests"
+    source:
+      repository: "https://github.com/your-org/your-test-repo.git"
+      branch: "main"
+      pull_interval: "60s"
+      webhook_enabled: false
+      webhook_secret: $["git-token-env-var-name"]
+  - name: "performance-test"
+    language: "javascript"
+    command: "npm run performance"
+    working_directory: "/app/performance"
+    source:
+      repository: "https://github.com/your-org/your-test-repo.git"
+      branch: "main"
+      pull_interval: "60s"
+      webhook_enabled: false
+      webhook_secret: $["git-token-env-var-name"]
+  - name: "database-test"
+    language: "bash"
+    command: "sh run_db_tests.sh"
+    working_directory: "/app/db"
+    source:
+      repository: "https://github.com/your-org/your-test-repo.git"
+      branch: "main"
+      pull_interval: "60s"
+      webhook_enabled: false
+      webhook_secret: $["git-token-env-var-name"]
 
 reporting:
-  endpoints: # Required: List of reporting endpoints
+  endpoints:
     - type: "datadog"
-      api_key: "your-datadog-api-key"
-      app_key: "your-datadog-app-key"
+      api_key: $["your-datadog-api-key"]
+      app_key: $["your-datadog-app-key"]
       metric_prefix: "app.monitor."
     - type: "s3"
       bucket: "your-s3-bucket"
       region: "us-east-1"
-      access_key_id: "your-access-key-id"
-      secret_access_key: "your-secret-access-key"
+      access_key_id: $["your-access-key-id"]
+      secret_access_key: $["your-secret-access-key"]
       path: "results/"
-    - type: "saas" # if using the SaaS service.
-      api_url: "https://your-saas-service.com/api/results"
-      api_token: "your-saas-api-token"
-  format: "json" # Optional: Format of the test results (e.g., "json", "xml", "text")
+    - type: "director"
+      api_url: "https://your-director-service.com/api/results"
+      api_token: "your-director-api-token"
+  format: "json"
 
-saas: # Configuration for SaaS integration (optional)
-  enabled: false # Optional: Enable SaaS integration (default: false)
-  api_url: "https://your-saas-service.com/api/runner" # Required if saas.enabled: true
-  runner_id: "unique-runner-id" # Required if saas.enabled: true
-  api_token: "your-saas-api-token" # Required if saas.enabled: true
+director:
+  enabled: false
+  api_url: "https://your-director-service.com/api/runner"
+  runner_id: $["unique-runner-id"]
+  api_token: $["your-director-api-token"]
 
 logging:
-  level: "INFO" # Optional: Logging level (e.g., "DEBUG", "INFO", "WARNING", "ERROR")
-  format: "text" # Optional: Logging format (e.g., "json", "text")
+  level: "INFO"
+  format: "text"
 ```
 
--- main.go
+++ main.go
@@ -2,21 +2,202 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"time"
 
+	"github.com/aranw/yamlcfg"
+
 	"github.com/go-co-op/gocron"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
+type Config struct {
+	Runner struct {
+		Name    string `yaml:"name"`
+		Version string `yaml:"version"`
+	} `yaml:"runner"`
+	Tests []TestConfig `yaml:"tests"`
+}
+
+type TestConfig struct {
+	Name   string `yaml:"name"`
+	Source struct {
+		Repository   string `yaml:"repository"`
+		Branch       string `yaml:"branch"`
+		PullInterval string `yaml:"pull_interval"`
+		GetUser      string `yaml:"get_user"`  // This will be an environment variable that should be substituted
+		GetToken     string `yaml:"get_token"` // This will be an environment variable that should be substituted
+	} `yaml:"source"`
+	Language  string `yaml:"language"`
+	Command   string `yaml:"command"`
+	Frequency int32  `yaml:"frequency"`
+}
+
 func main() {
+	// Bootstrap the application
+	config, err := bootstrap()
+	if err != nil {
+		fmt.Println("Error bootstrapping application:", err)
+		os.Exit(1)
+	}
+
+	// define a var to hold the tests
+	testConfigs := []TestConfig{}
+	for _, test := range config.Tests {
+		testConfigs = append(testConfigs, test)
+	}
+
+	// Schedule configuration updates
 	s := gocron.NewScheduler(time.UTC)
 
-	// Schedule the Git check job
-	s.Every("1m").Do(checkGitUpdates)
+	// Add the tests to the scheduler
+	for _, testConfig := range testConfigs {
+		s.Every(time.Duration(testConfig.Frequency) * time.Second).Do(func() {
+			// Pull the latest code from the repository
+			err := cloneOrUpdateRepo(testConfig.Source.Repository, testConfig.Source.GetUser, testConfig.Source.GetToken, testConfig.Name)
+			if err != nil {
+				fmt.Println("Error cloning/updating test repository:", err)
+				return
+			}
+			// Execute the test command
+			fmt.Printf("Executing test: %s\n", testConfig.Name)
+			// Here you would execute the test command, e.g., using os/exec
+			runString := fmt.Sprintf("cd /tmp/%s && %s", testConfig.Name, testConfig.Command)
+			cmd := exec.Command("bash", "-c", runString)
+			// err = cmd.Run()
+			// if err != nil {
+			// 	fmt.Printf("Error executing test command: %s\n", err)
+			// 	return
+			// }
+			// Print the output of the command
+			output, err := cmd.Output()
+			if err != nil {
+				fmt.Printf("Error executing test command: %s\n", err)
+				return
+			}
+			fmt.Printf("Output of test command: %s\n", output)
+			fmt.Printf("Test %s executed successfully\n", testConfig.Name)
+
+		})
+		fmt.Printf("Scheduled test: %s every %d seconds\n", testConfig.Name, testConfig.Frequency)
+	}
+
+	// s.Every(duration).Do(func() {
+	// 	// Implement logic to pull updates and update config
+	// 	fmt.Println("Checking for config updates...")
+	// 	// ...
+	// })
 
 	s.StartBlocking()
 }
 
-func checkGitUpdates() {
-	fmt.Println("Checking Git updates...")
-	// Implement your Git update logic here
+func cloneOrUpdateRepo(repoURL string, user string, token string, dirName string) error {
+	// Check if the directory exists
+	if _, err := os.Stat("/tmp/" + dirName); err == nil {
+		// Open the existing repository
+		repo, err := git.PlainOpen("/tmp/" + dirName)
+		if err != nil {
+			fmt.Println("Error opening repository:", err)
+			return err
+		}
+
+		// Pull the latest changes
+		w, err := repo.Worktree()
+		if err != nil {
+			fmt.Println("Error getting worktree:", err)
+			return err
+		}
+		err = w.Pull(&git.PullOptions{
+			Auth: &http.BasicAuth{
+				Username: user,
+				Password: token,
+			},
+		})
+		if err != nil {
+			if err.Error() != "already up-to-date" {
+				fmt.Println("Error pulling changes:", err)
+				return err
+			} else {
+				fmt.Println("Repository is already up-to-date")
+			}
+		} else {
+			fmt.Println("Successfully pulled latest changes")
+		}
+	} else if os.IsNotExist(err) {
+		// If the directory does not exist, clone the repository
+		repo, err := git.PlainClone("/tmp/"+dirName, false, &git.CloneOptions{
+			URL:      repoURL,
+			Progress: os.Stdout,
+			Auth: &http.BasicAuth{
+				Username: user,
+				Password: token,
+			},
+		})
+		if err != nil {
+			fmt.Println("Error cloning repository:", err)
+			return err
+		}
+		fmt.Println(repo)
+	}
+	return nil
+}
+
+func bootstrap() (Config, error) {
+	// Retrieve bootstrapping environment variables
+	configRepo := os.Getenv("RUNNER_CONFIG_REPO")
+	configBranch := os.Getenv("RUNNER_CONFIG_BRANCH")
+	if configBranch == "" {
+		configBranch = "main"
+	}
+	configPath := os.Getenv("RUNNER_CONFIG_PATH")
+	if configPath == "" {
+		configPath = "statusoptics.yaml"
+	}
+
+	gitConfigToken := os.Getenv("RUNNER_GIT_CONFIG_TOKEN")
+	if gitConfigToken == "" {
+		// Error out if the token is not provided
+		fmt.Println("Error: RUNNER_GIT_CONFIG_TOKEN environment variable is required")
+		return Config{}, fmt.Errorf("RUNNER_GIT_CONFIG_TOKEN environment variable is required")
+	}
+
+	gitConfigUser := os.Getenv("RUNNER_GIT_CONFIG_USER")
+	if gitConfigUser == "" {
+		// Error out if the user is not provided
+		fmt.Println("Error: RUNNER_GIT_CONFIG_USER environment variable is required")
+		return Config{}, fmt.Errorf("RUNNER_GIT_CONFIG_USER environment variable is required")
+	}
+
+	// Clone or update the config repository
+	err := cloneOrUpdateRepo(configRepo, gitConfigUser, gitConfigToken, "runner-config")
+	if err != nil {
+		fmt.Println("Error cloning/updating config repository:", err)
+		return Config{}, err
+	}
+
+	// Read and parse runner.yaml
+	config, err := readAndParseConfig("/tmp/runner-config/" + configPath)
+	if err != nil {
+		fmt.Println("Error reading/parsing config:", err)
+		return Config{}, err
+	}
+	fmt.Println(config)
+	return config, nil
+}
+
+func readAndParseConfig(filePath string) (Config, error) {
+	// read the file, unmarshal the yaml, and return the result.
+
+	cfg, err := yamlcfg.Parse[Config](filePath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	// print the parsed config for debugging
+	fmt.Printf("Parsed config: %+v\n", cfg)
+
+	return *cfg, nil
+
 }
